e5f3316f63acbc70ecd4ed47546fb8d6b3d86b0f
Nathan Arthur <narthur@preemptive.com>
2015-10-23 21:32:28 -0400

    fixes

--- vim-pathogen.sh
+++ vim-pathogen.sh
@@ -4,10 +4,12 @@ set -e
 
 mkdir -p ~/.vim/bundle
 cd ~/.vim/bundle/
+
 git clone https://github.com/tpope/vim-pathogen.git
+
 git clone https://github.com/airblade/vim-gitgutter.git
 git clone https://github.com/plasticboy/vim-markdown.git
-git clone http://git.devnull.li/ikiwiki-syntax.git
+git clone https://github.com/vim-scripts/ikiwiki-syntax
 git clone https://github.com/ervandew/supertab.git
 git clone https://github.com/PProvost/vim-ps1.git
 cd -
