7412a2fc7db7617518820c781c220efd465cf6b0
Nathan Arthur <narthur@preemptive.com>
2016-01-14 17:34:52 -0500

    guard against ssh security bug

--- configure_user.sh
+++ configure_user.sh
@@ -7,7 +7,7 @@ SETTINGS=src/settings
 
 clear_file() {
 	thefile="$1"
-	echo "$1..."
+	echo "  $1"
 	if [ -e "$thefile" ] || [ -L "$thefile" ]; then
 		if [ -f "$thefile" ]; then
 			mv "$thefile" "${thefile}.prior"
@@ -18,6 +18,7 @@ clear_file() {
 	fi
 }
 
+echo "dotfiles..."
 cd ~
 for nodotfile in bash_profile bashrc gitignore prompt_spec tmux.conf vimrc ; do
 	dotfile=".$nodotfile"
@@ -25,6 +26,8 @@ for nodotfile in bash_profile bashrc gitignore prompt_spec tmux.conf vimrc ; do
 	ln -s "$SETTINGS/$nodotfile" "$dotfile"
 done
 
+echo "gitconfig..."
+cd ~
 nodotfile="gitconfig"
 dotfile=".$nodotfile"
 clear_file "$dotfile"
@@ -33,6 +36,15 @@ cat << EOF > "$dotfile"
 	path = $SETTINGS/$nodotfile
 EOF
 
+echo "ssh UseRoaming..."
+cd ~
+sshdir=".ssh"
+mkdir -p "$sshdir"
+chmod 700 "$sshdir"
+echo -e '\nHost *\nUseRoaming no' >> "$sshdir/config"
+
+
+echo "bin scripts..."
 bindir="bin"
 relpath=".."
 mkdir -p "$bindir"
@@ -42,6 +54,7 @@ for scriptfile in cdp do_all master-to-new-branch session vif ; do
 	ln -s "$relpath/$SETTINGS/$scriptfile" "$scriptfile"
 done
 
+echo "vim plugins..."
 cd ~
 "$SETTINGS/vim-pathogen.sh"
 
