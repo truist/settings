0d0812a42a42a3184e000e2111447f006a537d11
Nathan Arthur <narthur-git@rainskit.com>
2021-09-05 23:55:30 -0400

    Total rewrite of git-fatfiles

--- bin/git-fatfiles.pl
+++ bin/git-fatfiles.pl
@@ -2,86 +2,160 @@
 
 use warnings;
 use strict;
+use v5.14;
 
 use Getopt::Long;
 use IPC::Open2;
-use v5.14;
+use List::Util qw(max sum);
 
-# based on https://stackoverflow.com/questions/1029969/why-is-my-git-repository-so-big/45366030#45366030
+# distantly based on the original answer at https://stackoverflow.com/questions/1029969/why-is-my-git-repository-so-big/45366030#45366030
 
-sub args {
-  say STDERR "Usage: $0 [--sum | -s] [--directories | -d] [--[no]human | -h]";
-  exit 1;
+sub usage {
+    STDERR->say("Usage: $0 [--sum | -s] [--directories | -d] [--[no]human | -h]");
+    exit 1;
 }
 
-my ($directories, $sum, $human);
-$human = 1;
-GetOptions('sum' => \$sum, 'directories' => \$directories, 'human!' => \$human)
-  or args();
-$sum = 1 if $directories;
+sub parseArgs {
+    my ($directories, $sum, $human);
 
-my $format_bytes = sub { return shift; };
-if ($human) {
-  # Try to get the "format_bytes" function:
-  my $canFormat = eval {
-      require Number::Bytes::Human;
-      Number::Bytes::Human->import('format_bytes');
-      1;
-  };
-  if ($canFormat) {
-      $format_bytes = \&format_bytes;
-  }
+    $human = 1;
+    GetOptions('sum' => \$sum, 'directories' => \$directories, 'human!' => \$human)
+        or usage();
+
+    if ($directories && !$sum) {
+        STDERR->say("Auto-enabling --sum because you used --directories");
+        $sum = 1;
+    }
+
+    return ($directories, $sum, $human);
 }
 
-my %revList = map { (split(' ', $_, 2))[0 => 1]; } qx(git rev-list --all --objects);
+my $format_bytes;
+sub formatBytes {
+    my ($human, $bytes) = @_;
 
-my $pid = open2(my $childOut, my $childIn, "git cat-file --batch-check");
-my %hashSizes = map {
-    print $childIn $_ . "\n";
-    my @blobData = split(' ', <$childOut>);
-    if ($blobData[1] eq 'blob') {
-        # [hash, size]
-        $blobData[0] => $blobData[2];
-    }
-    else {
-        ();
-    }
-} keys %revList;
-close($childIn);
-waitpid($pid, 0);
-
-sub trim { my $s = shift; $s =~ s/^\s+|\s+$//g; return $s };
-# Need to filter because some aren't files--there are useless directories in this list.
-# Format is name => size.
-# Also trim off trailing newlines in the names, at this point.
-my %fileSizes = map { exists($hashSizes{$_}) ? (trim($revList{$_}) => $hashSizes{$_}) : () } keys %revList;
-
-my @sortedSizes;
-if ($sum) {
-    my %fileSizeSums;
-    if ($directories) {
-        while (my ($name, $size) = each %fileSizes) {
-            # strip off the trailing part of the filename:
-            $fileSizeSums{$name =~ s|/[^/]*$||r} += $size;
-        }
-    }
-    else {
-        while (my ($name, $size) = each %fileSizes) {
-            $fileSizeSums{$name} += $size;
+    if ($human && !$format_bytes) {
+        # Try to get the "format_bytes" function:
+        my $canFormat = eval {
+            require Number::Bytes::Human;
+            Number::Bytes::Human->import('format_bytes');
+            1;
+        };
+        if ($canFormat) {
+            $format_bytes = \&format_bytes;
+        } else {
+            STDERR->say("Could not load Number::Bytes::Human; output will be in bytes");
         }
     }
 
-    @sortedSizes = map { [$_, $fileSizeSums{$_}] }
-        sort { $fileSizeSums{$a} <=> $fileSizeSums{$b} } keys %fileSizeSums;
-}
-else {
-    # Print the space taken by each file/blob, sorted by size
-    @sortedSizes = map { [$_, $fileSizes{$_}] }
-        sort { $fileSizes{$a} <=> $fileSizes{$b} } keys %fileSizes;
+    if (!$format_bytes) {
+        $format_bytes = sub { return shift; };
+    }
 
+    return $format_bytes->($bytes);
 }
 
-for my $fileSize (@sortedSizes) {
-    printf "%s\t%s\n", $format_bytes->($fileSize->[1]), $fileSize->[0];
+
+## ------------------------------- ##
+
+
+sub getGitObjects {
+    # hash => path
+    my %gitObjects;
+    for my $line (qx(git rev-list --all --objects)) {
+        my ($hash, $path) = split(' ', $line, 2);
+        $gitObjects{$hash} = $path =~ s|\s+$||r;  # trim trailing whitespace
+    }
+    return %gitObjects;
 }
+
+sub getHashSizesForBlobs {
+    my (@hashes) = @_;
+
+    # output is: "<hash> <type> <size>"
+    my $pid = open2(my $childOut, my $childIn, "git cat-file --batch-check");
+
+    # hash => size
+    my %hashSizes;
+    for my $hash (@hashes) {
+        $childIn->say($hash);
+
+        my ($hash, $type, $size) = split(' ', $childOut->getline());
+        if ($type eq 'blob') {
+            $hashSizes{$hash} = $size;
+        }
+    }
+
+    close($childIn);
+    waitpid($pid, 0);
+
+    return %hashSizes;
+}
+
+sub getAllSizesForPaths {
+    my ($gitObjects, $hashSizes) = @_;
+
+    # path => [size, ...]
+    my %pathSizes;
+    while (my ($hash, $path) = each %{$gitObjects}) {
+        # filter out directories
+        next unless exists $hashSizes->{$hash};
+
+        my $sizes = $pathSizes{$path} || [];
+        push(@{$sizes}, $hashSizes->{$hash});
+        $pathSizes{$path} = $sizes;
+    }
+
+    return %pathSizes;
+}
+
+sub mergeDirectories {
+    my (%pathSizes) = @_;
+
+    # dir => [size, ...]
+    my %dirSizes;
+    while (my ($path, $sizes) = each %pathSizes) {
+        my $pathOnly = $path =~ s|/[^/]*$||r;  # strip the filename part
+        if (exists $dirSizes{$pathOnly}) {
+            push(@{$sizes}, @{$dirSizes{$pathOnly}});
+        }
+        $dirSizes{$pathOnly} = $sizes;
+    }
+
+    return %dirSizes;
+}
+
+sub sumOrMaxSizes {
+    my ($sum, %pathSizes) = @_;
+
+    while (my ($path, $sizes) = each %pathSizes) {
+        $pathSizes{$path} = $sum ? sum(@{$sizes}) : max(@{$sizes});
+    }
+
+    return %pathSizes;
+}
+
+sub sortAndPrint {
+    my ($human, %pathSizes) = @_;
+
+    my @sortedPaths = sort { $pathSizes{$a} <=> $pathSizes{$b} } keys %pathSizes;
+    for my $path (@sortedPaths) {
+        printf("%s\t%s\n", formatBytes($human, $pathSizes{$path}), $path);
+    }
+}
+
+sub main {
+    my ($directories, $sum, $human) = parseArgs();
+
+    my %gitObjects = getGitObjects();
+    my %hashSizes = getHashSizesForBlobs(keys %gitObjects);
+    my %pathSizes = getAllSizesForPaths(\%gitObjects, \%hashSizes);
+
+    %pathSizes = mergeDirectories(%pathSizes) if $directories;
+    %pathSizes = sumOrMaxSizes($sum, %pathSizes);
+    sortAndPrint($human, %pathSizes);
+}
+
+main();
+
 
